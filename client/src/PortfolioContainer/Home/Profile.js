abfad3eae43a773a6c1f0fcb5effed339ab43413
JANNOT <ejannot@ensc.fr>
2022-05-15 23:28:26 +0200

    Add icons

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="#">
+              <i class="fa fa-brands fa-linkedin"></i>
+            </a>
+            <a href="#">
+              <i class="fa fa-brands fa-github-square"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
