e3ade93869f01c3a73a80ae96a2abb0e5d5104fb
JANNOT <ejannot@ensc.fr>
2022-05-16 00:04:52 +0200

    Text animation and resume btn

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import Typical from "react-typical";
 
 export default function Profile() {
   return (
@@ -6,11 +7,52 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="#">
+            <a href="https://www.linkedin.com/in/evan-jannot">
               <i class="fa fa-brands fa-linkedin"></i>
             </a>
-            <a href="#">
+            <a href="https://github.com/EvanJannot">
               <i class="fa fa-brands fa-github-square"></i>
+            </a>
+          </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {""}
+              Hello, I'm <span className="highlighted-text">Evan</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {""}
+              <h1>
+                {""}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Data Scientist 🧪",
+                    1000,
+                    "Full Stack Developer 💻",
+                    1000,
+                    "AI engineer ⚙️",
+                    1000,
+                    "UX Designer 🖌️",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Thinking technologies in relation to the human for the world of
+                tomorrow.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire me{""}
+            </button>
+            <a href="JannotCV.pdf" download="Evan Jannot CV.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
             </a>
           </div>
         </div>
